0cc002c197f2533211d8851e3966414b5c1632f9
kirtarget <sheldongriffiths1@gmail.com>
2022-08-11 07:21:21 +0300

    ...

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ import {
 
 ///////////////////////////////////////
 // Set Up
-const port = 3000
+const port = process.env.PORT || 3000
 const __dirname = path.resolve()
 // Подключение к БД
 mongoose
